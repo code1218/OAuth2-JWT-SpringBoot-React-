070bbf8b4f49c8eed851eebc03a204959e28c347
code1218 <skjil1218@gmail.com>
2023-05-16 17:42:37 +0900

    feat: 파일 업로드 완료

--- oauth2-back/src/main/java/com/study/oauth2/config/SecurityConfig.java
+++ oauth2-back/src/main/java/com/study/oauth2/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeRequests()
-			.antMatchers("/auth/**")
+			.antMatchers("/auth/**", "/image/**")
 			.permitAll()
 			.anyRequest()
 			.authenticated()
--- oauth2-front/src/pages/Posts/PostRegister.js
+++ oauth2-front/src/pages/Posts/PostRegister.js
@@ -1,12 +1,33 @@
 import React, { useRef, useState } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 import axios from 'axios';
 
 
 const PostRegister = () => {
+    const [ title, setTitle ] = useState("");
+    const [ content, setContent ] = useState("");
     const [ imgFiles, setImgFiles ] = useState([]);
     const fileId = useRef(1);
-    
+
+    const postRegisterSubmit = useMutation(async () => {
+        const formData = new FormData();
+        formData.append("title", title);
+        formData.append("userId", principal.data.data.userId);
+        formData.append("content", content);
+        
+        imgFiles.forEach(imgFile => {
+            formData.append("imgFiles", imgFile.file);
+        })
+
+        const option = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+                "Content-Type": "multipart/form-data" 
+            }
+        }
+        const response = await axios.post("http://localhost:8080/post/register", formData, option);
+        return response;
+    });
 
     const principal = useQuery(["principal"], async () => {
         const option = {
@@ -22,6 +43,14 @@ const PostRegister = () => {
         return <>...Loading</>;
     }
 
+    const titleOnChangeHandle = (e) => {
+        setTitle(e.target.value);
+    }
+
+    const contentOnChangeHandle = (e) => {
+        setContent(e.target.value);
+    }
+
     const addFileHandle = (e) => {
         const newImgFiles = [];
 
@@ -35,21 +64,37 @@ const PostRegister = () => {
         }
 
         setImgFiles([...imgFiles, ...newImgFiles]);
+        e.target.value = null;
+    }
+
+    const removeFileHandle = (e) => {
+        setImgFiles([...imgFiles.filter(imgFile => imgFile.id !== parseInt(e.target.value))]);
+    }
+
+    const registerPostSubmitHandle = () => {
+        
+        postRegisterSubmit.mutate();
     }
 
     return (
         <div>
             <h3>제목</h3>
-            <input type="text" />
+            <input type="text" onChange={titleOnChangeHandle} />
             <h3>작성자</h3>
             <input type="text" value={principal.data.data.name} disabled={true} />
             <h3>내용</h3>
-            <textarea cols="30" rows="10"></textarea>
+            <textarea cols="30" rows="10" onChange={contentOnChangeHandle}></textarea>
             <h3>이미지파일</h3>
-            <input type="file" multiple={true} onChange={addFileHandle} />
+            <input type="file" multiple={true} onChange={addFileHandle} accept={".jpg,.png"} />
             <ul>
-                {imgFiles.map(imgFile => <li key={imgFile.id}>{imgFile.file.name} <button>삭제</button></li>)}
+                {imgFiles.map(imgFile => 
+                    <li key={imgFile.id}>
+                        {imgFile.file.name} 
+                        <button value={imgFile.id} onClick={removeFileHandle}>삭제</button>
+                    </li>
+                )}
             </ul>
+            <button onClick={registerPostSubmitHandle}>작성하기</button>
         </div>
     );
 };
